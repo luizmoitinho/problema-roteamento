4c11c05fc0bba944f508f9d5b2d2f0c13013b79b
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2020-11-19 10:26:21 -0300

    ref - alteracao de nome dos metodos e classes

--- Main.py
+++ Main.py
@@ -1,29 +1,29 @@
-from Graph import Graph
+from Router import Router
 
-theGraph = Graph()
+theGraph = Router()
 
-theGraph.addVertex('A') # 0
-theGraph.addVertex('B') # 1
-theGraph.addVertex('C') # 2
-theGraph.addVertex('D') # 3
-theGraph.addVertex('E') # 4
-theGraph.addVertex('F') # 5
-theGraph.addVertex('G') # 6
+theGraph.addAccess('A') # 0
+theGraph.addAccess('B') # 1
+theGraph.addAccess('C') # 2
+theGraph.addAccess('D') # 3
+theGraph.addAccess('E') # 4
+theGraph.addAccess('F') # 5
+theGraph.addAccess('G') # 6
 
-theGraph.addEdge(0, 2, 14)
-theGraph.addEdge(0, 1, 12)
+theGraph.addLink(0, 2, 14)
+theGraph.addLink(0, 1, 12)
 
-theGraph.addEdge(1, 2, 9)
-theGraph.addEdge(1, 3, 38)
+theGraph.addLink(1, 2, 9)
+theGraph.addLink(1, 3, 38)
 
-theGraph.addEdge(2, 4, 7)
-theGraph.addEdge(2, 3, 24)
+theGraph.addLink(2, 4, 7)
+theGraph.addLink(2, 3, 24)
 
-theGraph.addEdge(3, 6, 9)
+theGraph.addLink(3, 6, 9)
 
-theGraph.addEdge(4, 3, 13)
-theGraph.addEdge(4, 6, 29)
-theGraph.addEdge(4, 5, 9)
+theGraph.addLink(4, 3, 13)
+theGraph.addLink(4, 6, 29)
+theGraph.addLink(4, 5, 9)
 
 #theGraph.getAdjUnvisitedVertex(0)
-print(theGraph.ucs(0, 3))
+print(theGraph.ucs(0, 5))
